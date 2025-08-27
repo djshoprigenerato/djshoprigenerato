e10cf631cbe6220587bba44effeadfb1a5b20cf7
djshoprigenerato <giampaandrea@yahoo.com>
2025-08-27 18:46:57 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ module.exports = {
     domains: ["xwduevuswmvlyfgwlkim.supabase.co"],
   },
   env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  }
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+}
 };
+
