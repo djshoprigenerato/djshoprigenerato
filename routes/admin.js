cb3459daa7bc807b8a1dcfafd99c59dbf3bf9d66
djshoprigenerato <giampaandrea@yahoo.com>
2025-08-27 18:25:36 +0200

    Delete routes directory

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Redirect to login or dashboard depending on your existing auth
-router.get('/', (req, res) => {
-  res.send('Admin placeholder: collega le rotte admin esistenti.');
-});
-
-export default router;
