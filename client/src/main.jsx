ffa21fae1c3561391d55660c628e4c583e6c4dbe
djshoprigenerato <giampaandrea@yahoo.com>
2025-09-07 13:30:44 +0200

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,5 +2,12 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.jsx"
 import "./index.css"
+import { HelmetProvider } from "react-helmet-async"
 
-createRoot(document.getElementById("root")).render(<App />)
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+)
